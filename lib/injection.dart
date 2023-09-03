3fc7e520b937d68109e1f537ec87433d48eaf6cc
sdsearle <spencer.seiro@gmail.com>
2023-09-03 15:28:20 -0700

    added instruction for injection.dart

--- lib/injection.dart
+++ lib/injection.dart
@@ -6,6 +6,8 @@ import 'package:flutter_back_bone/injection.config.dart';
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
 
+//generate with 'flutter pub run build_runner watch --delete-conflicting-outputs'
+
 final getIt = GetIt.instance;
 
 @InjectableInit(
